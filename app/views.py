16a62783f0af7a51d16a9be1bcdb2b95e1e2a1a0
Pablo Vallejo <pabloninety@gmail.com>
2016-06-14 14:10:06 -0500

    :sunny: Home: Filter based on user authentication status

--- app/views.py
+++ app/views.py
@@ -9,11 +9,48 @@ from .forms import SearchForm
 class HomeView(ListView):
     template_name = 'home.html'
     model = Category
-    context_object_name = 'category_list'
+
+    def get_queryset(self):
+        """ Returns an empty queryset as we're sending the
+            constructed data (category_list) in `get_context_data`.
+        """
+        return Category.objects.none()
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
-        context['form'] = SearchForm()
+
+        category_list = []
+        category_slugs = Category.objects.filter(
+            article__isnull=False,
+        ).distinct()
+
+        article_list = Article.objects.filter(
+            is_active=True,
+        )
+
+        # Creates an iterable structure that can be used in templates.
+        for category in category_slugs:
+            category_articles = article_list.filter(
+                category=category,
+            )
+
+            # If user is not authenticated, prevent
+            # `private` articles from being shown.
+            if not self.request.user.is_authenticated():
+                category_articles = category_articles.filter(
+                    is_private=False,
+                )
+
+            category_list.append([
+                category.name,
+                category.slug,
+                category_articles,
+            ])
+
+        context.update({
+            'category_list': category_list,
+            'form': SearchForm()
+        })
         return context
 
 
