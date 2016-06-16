3a06cda454205bbfe660df21be8959d2f80859aa
Camilo Nova <camilo.nova@axiacore.com>
2016-06-16 11:05:47 -0500

    Tweaks

--- app/tests.py
+++ app/tests.py
@@ -8,22 +8,22 @@ from .models import Article
 class CategoryTest(TestCase):
     def setUp(self):
         self.category = Category.objects.create(
-            name='Category A',
-            slug='catagory-a'
+            name='Category',
+            slug='category',
         )
 
 
 class ArticleTest(TestCase):
     def setUp(self):
         self.category = Category.objects.create(
-            name='Category A',
-            slug='catagory-a'
+            name='Category',
+            slug='category',
         )
 
         self.article_1 = Article.objects.create(
             category=self.category,
-            name='Article one name',
-            slug='article-one-name',
+            name='Active and public article',
+            slug='active-and-public',
             is_active=True,
             is_private=False,
             content='Article one content'
@@ -31,8 +31,8 @@ class ArticleTest(TestCase):
 
         self.article_2 = Article.objects.create(
             category=self.category,
-            name='Article two name',
-            slug='article-two-name',
+            name='Active and private article',
+            slug='active-and-private',
             is_active=True,
             is_private=True,
             content='Article two content'
@@ -40,8 +40,8 @@ class ArticleTest(TestCase):
 
         self.article_3 = Article.objects.create(
             category=self.category,
-            name='Article three name',
-            slug='article-three-name',
+            name='Non active article',
+            slug='non-active-article',
             is_active=False,
             is_private=False,
             content='Article three content'
@@ -49,30 +49,29 @@ class ArticleTest(TestCase):
 
         def test_public_article_view(self):
             response = self.client.get(reverse(
-                    'article_detail',
-                    args=[self.article_1.slug]
-                ))
+                'article_detail',
+                args=[self.article_1.slug]
+            ))
             self.assertEqual(response.status_code, 200)
 
         def test_unlogin_private_article_view(self):
             response = self.client.get(reverse(
-                    'article_detail',
-                    args=[self.article_2.slug]
-                ))
+                'article_detail',
+                args=[self.article_2.slug]
+            ))
             self.assertEqual(response.status_code, 400)
 
         def test_login_private_article_view(self):
             self.client.login(username='admin', password='demo')
             response = self.client.get(reverse(
-                    'article_detail',
-                    args=[self.article_2.slug]
-                ))
+                'article_detail',
+                args=[self.article_2.slug]
+            ))
             self.assertEqual(response.status_code, 200)
 
-
-        def test_unactive_article(self):
+        def test_inactive_article(self):
             response = self.client.get(reverse(
-                    'article_detail',
-                    args=[self.article_3.slug]
-                ))
+                'article_detail',
+                args=[self.article_3.slug]
+            ))
             self.assertEqual(response.status_code, 400)
