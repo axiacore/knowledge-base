edf78e0bc1e0e4334f77b5785749408ba426627d
Camilo Nova <camilo.nova@axiacore.com>
2018-11-19 10:57:20 -0500

    Removed unused setting

--- app/test_settings.py
+++ app/test_settings.py
@@ -12,22 +12,3 @@ SECRET_KEY = 'test'
 
 COMPRESS_ENABLED = False
 
-JENKINS_TASKS = (
-    'django_jenkins.tasks.run_pylint',
-    'django_jenkins.tasks.run_pep8',
-)
-
-PEP8_RCFILE = 'pep8.rc'
-
-
-class DisableMigrations(object):
-    """
-    Disable migrations when running tests
-    """
-    def __contains__(self, item):
-        return True
-
-    def __getitem__(self, item):
-        return "notmigrations"
-
-MIGRATION_MODULES = DisableMigrations()
